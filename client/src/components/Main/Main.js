30bc57936a40b33510a581f48bc43bbaa0b28577
DuskisNigh <s.ashmcc@gmail.com>
2019-05-24 14:32:47 -0400

    put dropdowns in main

--- client/src/components/Main/Main.js
+++ client/src/components/Main/Main.js
@@ -1,29 +1,17 @@
 import React, { Component } from 'react';
 import BarChart from '../BarChart';
-import PieChart from '../PieChart';
-import ScatterPlot from '../ScatterPlot';
-import WeatherGraph from '../WeatherGraph';
-import PineapplePie from '../PineapplePie';
-import MovieRatings from '../MovieRatings';
-
 import basicBarData from '../../data/basic-bar';
-import basicPieData from '../../data/basic-pie';
-import basicScatter from '../../data/basic-scatter';
-import weatherData from '../../data/weather-data';
-import pineappleData from '../../data/pineapple-on-pizza';
-import movieRatings from '../../data/movie-ratings';
 
 //new code
 import './Main.scss';
 import Dropdowns from './dropdowns.js'
 
-import {BrowserRouter as Router, Switch, Link, Route} from 'react-router-dom';
-
 class Main extends Component {
   render() {
     return (
-      <h1>hello form main</h1>
-      <Dropdowns />
+      <div>
+          <Dropdowns />
+      </div>
     )
   }
 }
